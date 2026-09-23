bb992db3b6f4c2928b1b10c50f8466c02de0a15b
tim <timm.kinipro@gmail.com>
2026-09-23 13:22:43 +0200

    Karte klasse

---
+++ Karte.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.Graphics.*;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.File;
+/**
+ * Beschreiben Sie hier die Klasse Karte.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Karte
+{
+    
+    
+    
+    /**
+     * Konstruktor für Objekte der Klasse Karte
+     */
+    public Karte()
+    {
+        // Instanzvariable initialisieren
+        
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return y;
+    }
+}
